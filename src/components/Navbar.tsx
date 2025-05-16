2007fc67912ea1ea273bed4c601ab4c67d99790f
Vanshaj Poonia <74147463+VanshajPoonia@users.noreply.github.com>
2025-05-16 12:12:52 +0000

    Updated nav link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,8 @@ import { useState } from "react";
 import { Menu, X } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { useIsMobile } from "@/hooks/use-mobile";
+import { Link } from 'react-router-dom';
+
 
 const Navbar = () => { 
   const [isMenuOpen, setIsMenuOpen] = useState(false);
